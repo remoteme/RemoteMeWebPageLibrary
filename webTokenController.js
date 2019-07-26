350a5319b7dcc05280bfe8a4b948fc31638d0e60
remoteme <maciej@mytimeplan.com>
2019-07-26 22:12:13 +0000

    next integration with webtoken

--- webTokenController.js
+++ webTokenController.js
@@ -1,4 +1,6 @@
 
+var webTokenLandingWebSocket;
+var expirationTime=undefined;
 
 
 window.onload=function () {
@@ -6,6 +8,13 @@ window.onload=function () {
 	setInterval(function(){
 		ping();
 	},1000);
+/*
+
+	webTokenLandingWebSocket = new WebSocket(getWebTokenLandingWebSocketAddress());
+	webTokenLandingWebSocket.binaryType = "arraybuffer";
+	webTokenLandingWebSocket.onmessage = onMessageWebTokenLandingWebSocket;
+*/
+
 
 };
 
@@ -21,4 +30,36 @@ function ping(){
 }
 
 
+
+
+
+
+function getWebTokenLandingWebSocketAddress(){
+	var ret;
+	if (window.location.protocol == 'https:') {
+		ret = "wss://";
+	} else {
+		ret = "ws://";
+	}
+	ret += window.location.host + "/innerWS/tokenLanding/";
+	return ret;
+
+}
+
+function onMessageWebTokenLandingWebSocket(event){
+	var dataJson = JSON.parse(event.data);
+	if (dataJson.type=="TOKEN_INFO_CHANGE"){
+		dataJson=JSON.parse(dataJson.dataS);
+		console.info(dataJson);
+		updateTimeAndCredit(dataJson);
+		if (dataJson.expirationTime==null){
+			redirectToTokenLanding();
+		}
+	}
+
+}
+function updateTimeAndCredit(dataJson){
+	expirationTime=dataJson.expirationTime
+
+}
 
